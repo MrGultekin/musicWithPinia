bc725142141d7d25183d2c1f1c8d1b318986825c
MrGultekin <gultekin@gmail.com>
2023-01-10 12:41:03 +0100

    116 Handled multiple error messages Password validation works.

--- src/components/AppAuth.vue
+++ src/components/AppAuth.vue
@@ -90,7 +90,6 @@
             </button>
           </form>
 
-
           <!-- Registration Form -->
           <vee-form
             v-show="tab === 'register'"
@@ -134,12 +133,21 @@
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
               <vee-field
-                type="password"
                 name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-              <ErrorMessage name="password" class="text-red-600" />
+                :bails="false"
+                v-slot="{ field, errors }"
+              >
+                <input
+                  type="password"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
+              <!--              <ErrorMessage name="password" class="text-red-600" />-->
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -195,17 +203,19 @@
 <script setup>
 import { useModelStore } from "@/stores/modalStore";
 import { ref } from "vue";
+
 const store = useModelStore();
 const tab = ref("login");
 const schema = ref({
   name: "required|min:3|max:100|alpha_spaces",
   email: "required|min:3|max:100|email",
   age: "required|max_value:100|min_value:18",
-  password: "required|min:3|max:100",
+  password: "required|min:9|max:100|excluded:password",
   confirm_password: "confirmed:@password",
   country: "required|excluded:Antarctica,Mantarctica",
   tos: "required",
 });
+
 function register(values) {
   console.log(values);
 }
